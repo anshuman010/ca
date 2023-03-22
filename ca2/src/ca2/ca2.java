8b5f5f8b8350cbb9d563bdfe427b1962766e5c31
anshuman010 <73747657+anshuman010@users.noreply.github.com>
2023-03-22 12:21:11 +0530

    Update ca2.java

--- ca2/src/ca2/ca2.java
+++ ca2/src/ca2/ca2.java
@@ -1,9 +1,28 @@
 package ca2;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ca2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Map creation of student and grades 
+		Map<String, String> hm = new HashMap<>();
+		 hm.put("John", "A");
+		 hm.put("mike","A");
+		 hm.put("Arjun", "A+");
+		 hm.put("Karan", "B");
+		 hm.put("Adi","C");
+		 hm.put("john", "B");
+		 for (Map.Entry<String, String> e : hm.entrySet())
+			 
+	            {System.out.println(e.getKey() + " "
+	                               + e.getValue());}
+		 //correction of grades
+		 hm.put("John", "C");
+		 for (Map.Entry<String, String> e : hm.entrySet())
+			 
+         {System.out.println(e.getKey() + " "
+                            + e.getValue());}
 
 	}
 
